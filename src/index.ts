7b59b2db90608a8a5829681bb8a619d08526371c
Vinicius Galvao de Franca <vinicius.gfranca98@gmail.com>
2021-11-03 20:51:26 -0300

    First Deploy Heroku

--- src/index.ts
+++ src/index.ts
@@ -6,8 +6,27 @@ import cors from 'cors';
 import * as bodyParser from "body-parser";
 import { createConnection } from 'typeorm';
 
+const config: any = {
+    "type": process.env.DB_DIALECT,
+    "host": process.env.DB_HOST,
+    "port": process.env.DB_PORT,
+    "username": process.env.DB_NAME,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_DEFAULT_DATABASE,
+    "entities": [
+       "src/app/models/*"
+    ],
+    "migrations": [
+       "src/database/migrations/*.ts"
+    ],
+    "cli": {
+    "migrationsDir": "src/database/migrations"
+        }
+    
+ }
+
 const app = express();
-createConnection();
+createConnection(config);
 app.use(cors())
 
 app.use(express.json());
