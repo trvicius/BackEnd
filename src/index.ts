eb5cdad2addcb3fa1cc26ffa4e4667eab15820b9
Vinicius Galvao de Franca <vinicius.gfranca98@gmail.com>
2021-11-03 20:36:27 -0300

    Commit Heroku

--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,13 @@ import './database/connect'
 import routes from './routes'
 import cors from 'cors';
 import * as bodyParser from "body-parser";
+import { createConnection } from 'typeorm';
 
 const app = express();
-
+createConnection();
 app.use(cors())
 
-app.use(bodyParser.json());
+app.use(express.json());
 app.use(routes)
 
-app.listen(4000, () => console.log('Server Started at http://localhost:4000'))
+app.listen(process.env.PORT || 4000, () => console.log('Server Started at http://localhost:4000'))
